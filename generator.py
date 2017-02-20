da98876e213b60b9f5e8697a1325111547e4c760
wangzhongfu <wangzhongfu@lakala.com>
2017-02-20 10:13:56 +0800

    生成器生成杨辉三角

---
+++ generator.py
@@ -0,0 +1,42 @@
+
+
+def triangles():
+    t = 0
+    last = []
+    tri = [1]
+    while True:
+        if t == 0:
+            yield tri
+            last = tri
+            t = 1
+            tri = list(range(t + 1))
+        elif t == 1:
+            for index in tri:
+                tri[0] = 1
+                tri[1] = 1
+            yield tri
+            last = tri
+            t = t + 1
+            tri = list(range(t + 1))
+        else:
+            for index in tri:
+                if index == 0:
+                    tri[index] = 1
+                    continue
+                if index == len(tri) - 1:
+                    tri[index] = 1
+                    continue
+                else:
+                    tri[index] = last[index] + last[index - 1]
+            yield tri
+            last = tri
+            t = t + 1
+            tri = list(range(t + 1))
+        #return 'done'
+
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 10:
+        break
